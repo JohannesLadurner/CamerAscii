6f8c3dc294873ba1a458d524f5600816eb5ebfb5
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2021-05-11 22:14:21 +0200

    added function to go through all the pixel groups + convert the pixels to binary

--- app/src/main/java/com/example/camerascii/ImageToAscii.kt
+++ app/src/main/java/com/example/camerascii/ImageToAscii.kt
@@ -24,7 +24,38 @@ class ImageToAscii {
             return pixels
         }
 
-        fun getBinaryPixel(pixel: Int, threshold: Int): Int {
+        private fun getAsciiImage(bitmap: Bitmap): Array<String?> {
+            var pixels = getBitmapPixels(bitmap)
+            var asciiImage = Array<String?>(bitmap.height) { null }
+
+            var row = 0
+            var pixelGroup = ""
+            var threshold:Double = 255.0/2.0
+            while(row < newBitmapHeight * newBitmapWidth){
+                var col = 0
+                while(col < newBitmapWidth) {
+                    //Upper left, Upper right
+                    pixelGroup += getBinaryPixel(pixels[row + col], threshold).toString()
+                    pixelGroup += getBinaryPixel(pixels[row + col + 1], threshold).toString()
+
+                    //Middle right, Middle left
+                    pixelGroup += getBinaryPixel(pixels[row + col + newBitmapWidth], threshold).toString()
+                    pixelGroup += getBinaryPixel(pixels[row + col + newBitmapWidth + 1], threshold).toString()
+
+                    //Bottom left, Bottom right
+                    pixelGroup += getBinaryPixel(pixels[row + col + newBitmapWidth * 2], threshold).toString()
+                    pixelGroup += getBinaryPixel(pixels[row + col + newBitmapWidth * 2 + 1], threshold).toString()
+
+                    col += 2
+                }
+                //Move 3 rows down
+                row += newBitmapWidth*3
+            }
+
+            return asciiImage
+        }
+
+        fun getBinaryPixel(pixel: Int, threshold: Double): Int {
 
             var brightness =
                 (0.2126 * Color.red(pixel) + 0.7152 * Color.green(pixel) + 0.0722 * Color.blue(pixel))
