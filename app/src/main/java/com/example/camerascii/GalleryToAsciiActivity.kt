ce98e63061b0d1e41c77cb3e8832817aed42c62a
Lukas-12 <lukas.buchsi@gmail.com>
2021-05-18 13:48:16 +0200

    GalleryToAsciiActivity.kt + XML created

---
+++ app/src/main/java/com/example/camerascii/GalleryToAsciiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.camerascii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GalleryToAsciiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery_to_ascii)
+    }
+}
