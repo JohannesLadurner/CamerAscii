a2f4d198e074470536be561bdc78bdd8f4e8db24
Lukas-12 <lukas.buchsi@gmail.com>
2021-05-24 16:30:40 +0200

    Add a about dialoge page

--- app/src/main/java/com/example/camerascii/StartActivity.kt
+++ app/src/main/java/com/example/camerascii/StartActivity.kt
@@ -1,11 +1,14 @@
 package com.example.camerascii
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 val SELECT_PICTURE = 200
 class StartActivity : AppCompatActivity() {
@@ -27,6 +30,10 @@ class StartActivity : AppCompatActivity() {
             startActivityForResult(i, SELECT_PICTURE);
 
         }
+
+        findViewById<FloatingActionButton>(R.id.infoButton).setOnClickListener {
+            showDialoge()
+        }
     }
     /*
     Calls, when you select a picture from the gallery
@@ -39,6 +46,28 @@ class StartActivity : AppCompatActivity() {
                 intent.putExtra("Picture",imageUri)
                 startActivity(intent)
 
+        }
+    }
+
+    fun showDialoge(){
+        val dia = Dialog(this)
+        dia.setContentView(R.layout.about_dialoge)
+        dia.show()
+
+        dia.findViewById<FloatingActionButton>(R.id.mailFab).setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "plain/text"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("lukasbu@edu.aau.at"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, "CameraAscii")
+            intent.putExtra(Intent.EXTRA_TEXT, "mail body")
+            startActivity(Intent.createChooser(intent, ""))
+
+        }
+        dia.findViewById<FloatingActionButton>(R.id.gitFab).setOnClickListener {
+            val browserIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://github.com/JohannesLadurner/CamerAscii"))
+            startActivity(browserIntent)
         }
     }
 }
